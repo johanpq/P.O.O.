9f7b2ac05c91083edb85fea9bf138a395d18a220
johanpq <johanp.queiroz@gmail.com>
2024-06-04 09:36:51 -0300

    Questão 3

--- lista/Questao3.java
+++ lista/Questao3.java
@@ -13,7 +13,15 @@ public class Questao3 {
 
         double converterRadianos = pickAnguloEmGraus * (Math.PI / 180);
 
+        double seno = Math.sin(converterRadianos);
+        double cosseno = Math.cos(converterRadianos);
+        double tangente = Math.tan(converterRadianos);
+        double cossecante = 1 / converterRadianos;
+        double secante = 1 / converterRadianos;
+        double cotangente = 1 / converterRadianos;
+
         System.out.printf("O angulo convertido eh: %g", converterRadianos);
+        System.out.printf("Seno: %g\nCosseno: %g\nTangente: %g\nCossecante: %g\nSecante: %g\nCotangente: %g", seno, cosseno, tangente, cossecante, secante, cotangente);
     }
 }
 
