c3ed9e435d3cc5211989b408db120edc67aca733
johanpq <johanp.queiroz@gmail.com>
2024-06-15 14:49:17 -0300

    Questão 10

---
+++ lista/Questao10.java
@@ -0,0 +1,32 @@
+package lista;
+
+public class Questao10 {
+    public static void main(String[] args) {
+        /*
+         Determine se cada uma das seguintes alternativas é verdadeira ou falsa. Se falsa, explique por quê.
+• O caso default é requerido na instrução de seleção switch.
+• A instrução break é requerida no último caso de uma instrução de seleção switch.
+• A expressão ((x > y)&&(a < b)) é verdadeira se (x > y) for verdadeiro ou (a < b) for
+verdadeira.
+2
+• Uma expressão contendo o operador || é verdadeira se um ou ambos de seus operandos forem verdadeiros.
+• Para testar para uma série de valores em uma instrução switch, pode-se utilizar um
+hífen ( - ) entre os valores inicial e final da série em um rótulo case.
+Para testar para uma série de valores em uma instrução switch, pode-se utilizar um
+hífen ( - ) entre os valores inicial e final da série em um rótulo case.
+• Listar casos consecutivamente sem instruções entre eles permite aos casos executar o mesmo conjunto de instruções.
+         */
+    }
+}
+
+// 1. Verdadeiro. O caso default sim é requerido, pois caso o valor de entrada não seja como esperado, o default poderá manipular, caso não tenha o default, então o código poderá ter erros, pois o usuário pode colocar qualquer coisa, mesmo guiando ele para as possíveis respostas para receber um feedback do sistema.
+
+// 2. Falso, caso feito no último case, então todos os outros cases acima serão executados e só irá para o código quando chegar no último break.
+
+// 3. Falso, só será verdadeira se ambas forem verdadeiras, ou seja, se ambas satisfazerem a condição.
+
+// 4. Falso, é verdadeiro se um ou outro for verdadeiro.
+
+// 5. Falso. Porque varia de linguagem para linguagem.
+
+// 6. Verdadeiro.
