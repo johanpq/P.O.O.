68f8264a52469c2336ba9dd61d92cc76da5babb0
johanpq <johanp.queiroz@gmail.com>
2024-08-22 07:01:55 -0300

    fixed

--- motivacao/Veiculos/Veiculo.java
+++ motivacao/Veiculos/Veiculo.java
@@ -10,4 +10,4 @@ public abstract class Veiculo { // Se uma class é abstrata, não é possível i
     public abstract void moverFrente();
 }
 
-/*/*Uma classe abstrata é uma classe que não pode ser instanciada diretamente; ela deve ser estendida por outras classes. Você usa classes abstratas quando deseja criar uma classe base que fornece algum comportamento padrão, mas deixa partes da implementação para as subclasses. */ */
+/*Uma classe abstrata é uma classe que não pode ser instanciada diretamente; ela deve ser estendida por outras classes. Você usa classes abstratas quando deseja criar uma classe base que fornece algum comportamento padrão, mas deixa partes da implementação para as subclasses. */ 
